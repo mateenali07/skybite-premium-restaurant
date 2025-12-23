4131fd92792e67449e06e70c36e1e302fec88fce
mateenali07 <mateenaliabro77@gmail.com>
2025-12-23 22:44:53 +0500

    Fix: Remove legacy SQLite causing production crashes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const db = require('./db/database');
+
 
 const apiRoutes = require('./routes/api');
 const authRoutes = require('./routes/authRoutes');
